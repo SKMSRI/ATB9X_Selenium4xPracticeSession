2f8e9d267405b5cf355b3b3e68706ef43e08a6f3
SAURABH <er.saurabhkumar22@gmail.com>
2025-01-02 19:42:11 +0530

    02 Jan

--- src/test/java/com/Selenium4xPractice/SeleniumBasics/TestWebDriver01.java
+++ src/test/java/com/Selenium4xPractice/SeleniumBasics/TestWebDriver01.java
@@ -4,10 +4,12 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TestWebDriver01 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         FirefoxDriver driver = new FirefoxDriver();
         driver.get("https://app.vwo.com");
+        Thread.sleep(5000);
+        driver.manage().window().maximize();
         System.out.println(driver.getTitle());
     }
 }
