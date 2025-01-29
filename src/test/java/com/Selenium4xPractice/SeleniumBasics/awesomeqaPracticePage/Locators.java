0e507144fc1596c955bc1c6697b792d22d36ed6f
SAURABH <er.saurabhkumar22@gmail.com>
2025-01-29 06:19:09 +0530

    29 Jan

--- src/test/java/com/Selenium4xPractice/SeleniumBasics/awesomeqaPracticePage/Locators.java
+++ src/test/java/com/Selenium4xPractice/SeleniumBasics/awesomeqaPracticePage/Locators.java
@@ -1,5 +1,6 @@
 package com.Selenium4xPractice.SeleniumBasics.awesomeqaPracticePage;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.edge.EdgeDriver;
 
 public class Locators {
@@ -10,6 +11,22 @@ public class Locators {
         driver.get("https://awesomeqa.com/practice.html");
         driver.manage().window().maximize();
         Thread.sleep(5000);
+
+        //Text box
+        driver.findElement(By.xpath("//input[@name = 'firstname']")).sendKeys("SAURABH");
+        driver.findElement(By.xpath("//input[@name = 'lastname']")).sendKeys("SRIVASTAVA");
+
+        //Radio button
+        driver.findElement(By.xpath("//input[contains(@id,'sex-0')]")).click();
+        driver.findElement(By.xpath("//input[contains(@id,'exp-4')]")).click();
+
+        //text box for date
+        driver.findElement(By.xpath("//input[contains(@id, 'datepicker')]")).sendKeys("29-Jan-2025");
+
+        //checkbox
+        driver.findElement(By.xpath("//input[contains(@value,'Automation Tester')]")).click();
+        driver.findElement(By.xpath("//input[contains(@value,'Selenium Webdriver')]")).click();
+
 
 
 
